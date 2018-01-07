1962c5c5bb441f675b9721de5d396636b0be99cd
EveV1 <eve.veltmann@gmail.com>
2018-01-07 16:40:39 +0200

    Update plot3.R
    legend

--- plot3.R
+++ plot3.R
@@ -16,7 +16,7 @@ with(data_in,plot(Date, Sub_metering_1, type = "l", col="black",
                   ylab = "Energy sub metering", xlab = "" ))
 lines(data_in$Date, data_in$Sub_metering_2, col="red")
 lines(data_in$Date, data_in$Sub_metering_3, col="blue")
-legend("topright",legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
-       col = c("black", "red", "blue"), lty = 1)
+legend("topright",legend = c("Sub_metering_1  ", "Sub_metering_2  ", "Sub_metering_3  "),
+       col = c("black", "red", "blue"), lty = 1, cex=0.9)
 dev.copy(png, file = "plot3.png" )
 dev.off() 
